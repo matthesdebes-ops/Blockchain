307fe886c07083c0673feb2932dbee8e8b5221da
Aldouse Vanderboom <matthesdebes@gmail.com>
2026-06-16 15:20:49 +0200

    deleted

--- Project async plus web3.py
+++ Project async plus web3.py
@@ -2,6 +2,7 @@
 import asyncio
 import aiohttp
 from web3 import Web3
+from web3 import AsyncWeb3
 
 RPC_URL = "https://mainnet.era.zksync.io"
 TOKEN_ADDRESS = "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4"
